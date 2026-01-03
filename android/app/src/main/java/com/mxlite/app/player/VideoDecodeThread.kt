e1c1fd017a29eb06fb768376f462ef5b51486de2
Modal CI <ci@modal.run>
2026-01-03 23:11:10 +0000

    Step R: Add audio-clock synced video frame queue (AV sync)

--- android/app/src/main/java/com/mxlite/app/player/VideoDecodeThread.kt
+++ android/app/src/main/java/com/mxlite/app/player/VideoDecodeThread.kt
@@ -2,6 +2,7 @@
 package com.mxlite.app.player
 
 class VideoDecodeThread(
+    private val frameQueue: VideoFrameQueue,
     private val player: FFmpegPlayer,
     private val renderer: VideoRenderer
 ) : Thread("VideoDecodeThread") {
@@ -10,7 +11,10 @@ class VideoDecodeThread(
     private var running = true
 
     override fun run() {
+        
         while (running) {
+            val audioClock = audioRenderer.getClockMs()
+
             val frame = player.nativeReadVideoFrame() ?: continue
             renderer.render(frame)
         }
