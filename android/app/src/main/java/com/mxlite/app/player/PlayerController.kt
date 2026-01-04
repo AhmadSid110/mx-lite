8de1b5dc1b738ab8f711d9a5eb80905e6b592bfe
Mohammad Ahmad Siddiqui <ahmad.sid2049@gmail.com>
2026-01-05 04:46:51 +0530

    Implement PlayerController for audio and video playback

---
+++ android/app/src/main/java/com/mxlite/app/player/PlayerController.kt
@@ -0,0 +1,43 @@
+package com.mxlite.app.player
+
+import android.view.Surface
+import java.io.File
+
+class PlayerController : PlayerEngine {
+
+    private val audio = AudioCodecEngine()
+    private val video = MediaCodecEngine(clock = audio)
+
+    override val durationMs: Long
+        get() = video.durationMs
+
+    override val currentPositionMs: Long
+        get() = audio.positionMs
+
+    override val isPlaying: Boolean
+        get() = video.isPlaying
+
+    override fun attachSurface(surface: Surface) {
+        video.attachSurface(surface)
+    }
+
+    override fun play(file: File) {
+        audio.reset()
+        audio.play()
+        video.play(file)
+    }
+
+    override fun pause() {
+        audio.pause()
+        video.pause()
+    }
+
+    override fun seekTo(positionMs: Long) {
+        // D2-D+
+    }
+
+    override fun release() {
+        audio.pause()
+        video.release()
+    }
+}
