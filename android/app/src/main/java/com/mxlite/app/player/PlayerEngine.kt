9c9dd5a687e74b2f7a5b29666b267b8d2d8da02a
Mohammad Ahmad Siddiqui <ahmad.sid2049@gmail.com>
2026-01-06 08:47:25 +0530

    Remove SAF playback function from PlayerEngine
    Removed SAF playback function from PlayerEngine interface.

--- android/app/src/main/java/com/mxlite/app/player/PlayerEngine.kt
+++ android/app/src/main/java/com/mxlite/app/player/PlayerEngine.kt
@@ -1,20 +1,18 @@
 package com.mxlite.app.player
 
-import android.net.Uri
 import android.view.Surface
 import java.io.File
 
 interface PlayerEngine {
+
     fun attachSurface(surface: Surface)
 
-    // Filesystem playback
     fun play(file: File)
 
-    // SAF playback (NEW)
-    fun play(uri: Uri)
-
     fun pause()
+
     fun seekTo(positionMs: Long)
+
     fun release()
 
     val durationMs: Long
