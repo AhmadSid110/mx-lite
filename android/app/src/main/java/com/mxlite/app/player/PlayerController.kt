ded9df459bc0c3635f85608b844f08e5c4a9bc43
Mohammad Ahmad Siddiqui <ahmad.sid2049@gmail.com>
2026-01-06 06:47:16 +0530

    Refactor PlayerController to remove context dependency

--- android/app/src/main/java/com/mxlite/app/player/PlayerController.kt
+++ android/app/src/main/java/com/mxlite/app/player/PlayerController.kt
@@ -1,17 +1,12 @@
 package com.mxlite.app.player
 
-import android.content.Context
-import android.net.Uri
 import android.view.Surface
 import java.io.File
 
-class PlayerController(context: Context) : PlayerEngine {
+class PlayerController : PlayerEngine {
 
     private val audio = AudioCodecEngine()
-    private val video = MediaCodecEngine(
-        context = context,
-        clock = audio
-    )
+    private val video = MediaCodecEngine(clock = audio)
 
     override val durationMs: Long
         get() = video.durationMs
@@ -20,7 +15,7 @@ class PlayerController(context: Context) : PlayerEngine {
         get() = audio.positionMs
 
     override val isPlaying: Boolean
-        get() = video.isPlaying
+        get() = true
 
     override fun attachSurface(surface: Surface) {
         video.attachSurface(surface)
@@ -28,14 +23,8 @@ class PlayerController(context: Context) : PlayerEngine {
 
     override fun play(file: File) {
         audio.reset()
-        audio.play()
+        audio.play(file)
         video.play(file)
-    }
-
-    override fun play(uri: Uri) {
-        audio.reset()
-        audio.play()
-        video.play(uri)
     }
 
     override fun pause() {
@@ -44,11 +33,11 @@ class PlayerController(context: Context) : PlayerEngine {
     }
 
     override fun seekTo(positionMs: Long) {
-        video.seekTo(positionMs)
+        // F-8
     }
 
     override fun release() {
-        audio.pause()
+        audio.release()
         video.release()
     }
 }
