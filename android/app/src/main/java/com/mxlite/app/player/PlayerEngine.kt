0e1f7ef9d58f83acd7d80bc279088ac5c9779e5f
Mohammad Ahmad Siddiqui <ahmad.sid2049@gmail.com>
2026-01-06 05:55:45 +0530

    Add play function overload for Uri parameter

--- android/app/src/main/java/com/mxlite/app/player/PlayerEngine.kt
+++ android/app/src/main/java/com/mxlite/app/player/PlayerEngine.kt
@@ -1,11 +1,15 @@
 package com.mxlite.app.player
 
+import android.net.Uri
 import android.view.Surface
 import java.io.File
 
 interface PlayerEngine {
     fun attachSurface(surface: Surface)
+
     fun play(file: File)
+    fun play(uri: Uri)   // 👈 ADD THIS
+
     fun pause()
     fun seekTo(positionMs: Long)
     fun release()
