c2c1379e75ecf870a23fb368dc69b85623313aa6
Mohammad Ahmad Siddiqui <ahmad.sid2049@gmail.com>
2026-01-06 08:44:26 +0530

    Add URI playback method to PlayerEngine interface
    Added a new play method to support URI playback.

--- android/app/src/main/java/com/mxlite/app/player/PlayerEngine.kt
+++ android/app/src/main/java/com/mxlite/app/player/PlayerEngine.kt
@@ -1,18 +1,20 @@
 package com.mxlite.app.player
 
+import android.net.Uri
 import android.view.Surface
 import java.io.File
 
 interface PlayerEngine {
-
     fun attachSurface(surface: Surface)
 
+    // Filesystem playback
     fun play(file: File)
 
+    // SAF playback (NEW)
+    fun play(uri: Uri)
+
     fun pause()
-
     fun seekTo(positionMs: Long)
-
     fun release()
 
     val durationMs: Long
