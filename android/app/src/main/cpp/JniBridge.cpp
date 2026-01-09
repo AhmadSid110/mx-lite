0cf8fbfd30ece96f6b42998217cdfe4e5fccec9d
Mohammad Ahmad Siddiqui <ahmad.sid2049@gmail.com>
2026-01-09 23:30:31 +0530

    Update JniBridge.cpp

--- android/app/src/main/cpp/JniBridge.cpp
+++ android/app/src/main/cpp/JniBridge.cpp
@@ -1,10 +1,6 @@
 #include <jni.h>
-#include <android/log.h>
 #include "player/AudioEngine.h"
 #include "player/Clock.h"
-
-#define LOG_TAG "JniBridge"
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
 
 static Clock gClock;
 static AudioEngine* gAudio = nullptr;
@@ -15,15 +11,12 @@ Java_com_mxlite_app_player_NativePlayer_nativePlay(
         JNIEnv* env, jobject, jstring path) {
 
     const char* cpath = env->GetStringUTFChars(path, nullptr);
-    LOGD("nativePlay: %s", cpath);
 
-    if (!gAudio) {
+    if (!gAudio)
         gAudio = new AudioEngine(&gClock);
-    }
 
-    if (gAudio->open(cpath)) {
+    if (gAudio->open(cpath))
         gAudio->start();
-    }
 
     env->ReleaseStringUTFChars(path, cpath);
 }
@@ -37,31 +30,24 @@ Java_com_mxlite_app_player_NativePlayer_nativeGetClockUs(
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_mxlite_app_player_NativePlayer_nativeStop(
-        JNIEnv*, jobject) {
-    LOGD("nativeStop");
-    if (gAudio) {
-        gAudio->stop();
-    }
+Java_com_mxlite_app_player_NativePlayer_nativeSeek(
+        JNIEnv*, jobject, jlong posUs) {
+    if (gAudio)
+        gAudio->seekUs((int64_t)posUs);
 }
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_com_mxlite_app_player_NativePlayer_nativeSeek(
-        JNIEnv*, jobject, jlong positionUs) {
-    LOGD("nativeSeek: %lld", (long long)positionUs);
-    if (gAudio) {
-        gAudio->seekUs(positionUs);
-    }
+Java_com_mxlite_app_player_NativePlayer_nativeStop(
+        JNIEnv*, jobject) {
+    if (gAudio)
+        gAudio->stop();
 }
 
 extern "C"
 JNIEXPORT void JNICALL
 Java_com_mxlite_app_player_NativePlayer_nativeRelease(
         JNIEnv*, jobject) {
-    LOGD("nativeRelease");
-    if (gAudio) {
-        delete gAudio;
-        gAudio = nullptr;
-    }
+    delete gAudio;
+    gAudio = nullptr;
 }
