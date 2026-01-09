54a9b055b0f6ddd7705ff27804667820d106e8f6
Mohammad Ahmad Siddiqui <ahmad.sid2049@gmail.com>
2026-01-10 01:03:00 +0530

    Update Clock.h

--- android/app/src/main/cpp/player/Clock.h
+++ android/app/src/main/cpp/player/Clock.h
@@ -1,10 +1,17 @@
 #pragma once
+
 #include <atomic>
 #include <cstdint>
 
 class Clock {
 public:
+    // Absolute set (used on seek / start)
     void setUs(int64_t us);
+
+    // Incremental add (used during playback)
+    void addUs(int64_t deltaUs);
+
+    // Read current clock
     int64_t getUs() const;
 
 private:
