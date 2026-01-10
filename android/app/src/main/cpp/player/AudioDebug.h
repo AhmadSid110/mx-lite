488d04d94dc5fec7af3c29e97834d79042086eff
Mohammad Ahmad Siddiqui <ahmad.sid2049@gmail.com>
2026-01-10 18:24:28 +0000

    Update AudioDebug.h

--- android/app/src/main/cpp/player/AudioDebug.h
+++ android/app/src/main/cpp/player/AudioDebug.h
@@ -1,11 +1,22 @@
 #pragma once
 #include <atomic>
+#include <cstdint>
 
 struct AudioDebug {
+    // Lifecycle
     std::atomic<bool> engineCreated{false};
+
+    // AAudio state
     std::atomic<bool> aaudioOpened{false};
     std::atomic<bool> aaudioStarted{false};
+
+    // Callback health
     std::atomic<bool> callbackCalled{false};
+    std::atomic<int64_t> callbackCount{0};
+
+    // Decoder
     std::atomic<bool> decoderProduced{false};
-    std::atomic<int> bufferFill{0};
+
+    // Ring buffer state (frames)
+    std::atomic<int64_t> bufferFill{0};
 };
